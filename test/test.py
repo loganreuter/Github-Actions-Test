0b182c7604f3746029c48ca9994e4a2b35870aad
lreuter2020 <lreuter2020@gmail.com>
2023-08-15 21:28:54 -0700

    updated tests

---
+++ test/test.py
@@ -0,0 +1,13 @@
+import unittest
+
+from src.main import *
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2,2), 4)
+    
+    def test_sub(self):
+        self.assertEqual(sub(2,2), 0)
+    
+    def test_mul(self):
+        self.assertEqual(mul(2,2), 3)
