89f86c3a1f06fb2552606c0a9e4a84141542cbfb
lreuter2020 <lreuter2020@gmail.com>
2023-08-15 20:29:21 -0700

    fixed bug in a test

--- test/test_add.py
+++ test/test_add.py
@@ -1,11 +1,11 @@
 import unittest
-from src.main import add
+from src.main import add, sub
 
 class TestCalculations(unittest.TestCase):
-    def test_add_a(self):
+    def test_add(self):
         val = add(1,1)
         self.assertEqual(val, 2, "Should be 2")
     
-    def test_add_b(self):
-        val = add(1,1)
-        self.assertEqual(val, 1, "Should be 1")
+    def test_sub(self):
+        val = sub(1,1)
+        self.assertEqual(val, 0, "Should be 0")
